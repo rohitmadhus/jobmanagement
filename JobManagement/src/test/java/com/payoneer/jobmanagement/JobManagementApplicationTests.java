56bd4473ce21c16f4cef24b71a4eb39b036998dd
Rohit Madhu <rohitmadhu@Rohits-MacBook-Pro.local>
2021-09-30 00:20:41 +0530

    back end

---
+++ JobManagement/src/test/java/com/payoneer/jobmanagement/JobManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payoneer.jobmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
